19d612d7bc9c4eb9fbe50001c496f3d72519ba38
Salakar <mike.diarmid@gmail.com>
2020-05-22 10:42:18 +0100

    disable version check

--- script/incremental_build.sh
+++ script/incremental_build.sh
@@ -52,6 +52,7 @@ else
   else
     (cd "$REPO_DIR" && pub global run flutter_plugin_tools "${ACTIONS[@]}" --plugins="$CHANGED_PACKAGES" $PLUGIN_SHARDING)
     echo "Running version check for changed packages"
-    (cd "$REPO_DIR" && pub global run flutter_plugin_tools version-check --base_sha="$(get_branch_base_sha)")
+    # TODO(salakar): commenting out version check - will be replaced with auto versioning through melos
+    # (cd "$REPO_DIR" && pub global run flutter_plugin_tools version-check --base_sha="$(get_branch_base_sha)")
   fi
 fi

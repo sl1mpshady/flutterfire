d8f9d88f82a7dc8184119a1dc4350e49d666ff81
Helena Ford <helenaellieford@gmail.com>
2020-05-26 15:09:31 +0100

    cloud functions tests

--- packages/cloud_functions/cloud_functions/test/test_common.dart
+++ packages/cloud_functions/cloud_functions/test/test_common.dart
@@ -1,25 +1,13 @@
-// Copyright 2017, the Chromium project authors.  Please see the AUTHORS file
+// Copyright 2020, the Chromium project authors.  Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'package:cloud_functions_platform_interface/cloud_functions_platform_interface.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:firebase_core_platform_interface/firebase_core_platform_interface.dart';
 
-typedef MethodCallCallback = dynamic Function(MethodCall methodCall);
-
-const kCollectionId = "test";
-const kDocumentId = "document";
-
 void initializeMethodChannel() {
-  // Install the Codec that is able to decode FieldValues.
-  MethodChannelCloudFunctions.channel = MethodChannel(
-    'plugins.flutter.io/cloud_firestore',
-    StandardMethodCodec(TestFunctionsMessageCodec()),
-  );
-
   TestWidgetsFlutterBinding.ensureInitialized();
+
   MethodChannelFirebaseCore.channel.setMockMethodCallHandler((call) async {
     if (call.method == 'FirebaseCore#initializeCore') {
       return [
@@ -45,11 +33,3 @@ void initializeMethodChannel() {
     return null;
   });
 }
-
-void handleMethodCall(MethodCallCallback methodCallCallback) =>
-    MethodChannelCloudFunctions.channel.setMockMethodCallHandler((call) async {
-      expect(
-          call.arguments["app"], equals(FunctionsPlatform.instance.app.name));
-      expect(call.arguments["path"], equals("$kCollectionId/$kDocumentId"));
-      return await methodCallCallback(call);
-    });

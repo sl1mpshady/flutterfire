54c8ee32ec9c039eaff73ffd08bc4421420b1a61
Helena Ford <helenaellieford@gmail.com>
2020-05-26 23:17:41 +0100

    cloud functions example e2e test

--- packages/cloud_functions/cloud_functions/example/test_driver/cloud_functions_e2e.dart
+++ packages/cloud_functions/cloud_functions/example/test_driver/cloud_functions_e2e.dart
@@ -1,46 +1,12 @@
-import 'dart:async';
-
 import 'package:e2e/e2e.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:cloud_functions_example/main.dart';
-import 'dart:async';
-import 'package:flutter_driver/driver_extension.dart';
-import 'package:flutter_test/flutter_test.dart';
 import 'package:cloud_functions/cloud_functions.dart';
-// void main() {
-//   E2EWidgetsFlutterBinding.ensureInitialized();
-
-//   group('$CloudFunctions', () {
-//     final CloudFunctions cloudFunctions = CloudFunctions.instance;
-
-//     testWidgets('runTransaction', (WidgetTester tester) async {});
-//   });
-// }
-// Future<void> main() async {
-//   E2EWidgetsFlutterBinding.ensureInitialized();
-//   await FirebaseCore.instance.initializeApp();
-//   // final Completer<String> completer = Completer<String>();
-//   // enableFlutterDriverExtension(handler: (_) => completer.future);
-//   // tearDownAll(() => completer.complete(null));
-// }
 
 void main() {
   E2EWidgetsFlutterBinding.ensureInitialized();
 
-  // testWidgets('CloudFunctions example widget test',
-  //     (WidgetTester tester) async {
-  //   await tester.pumpWidget(MyApp());
-  //   expect(find.text('Cloud Functions example app'), findsOneWidget);
-  // });
-
   testWidgets('call', (WidgetTester tester) async {
-    await FirebaseCore.instance.initializeApp();
-    //   Firestore firestore = Firestore();
-    //   expect(firestore, isNotNull);
-    // });
-
-    // test('call', () async {
     await FirebaseCore.instance.initializeApp();
     final HttpsCallable callable =
         CloudFunctions.instance.getHttpsCallable(functionName: 'repeat');

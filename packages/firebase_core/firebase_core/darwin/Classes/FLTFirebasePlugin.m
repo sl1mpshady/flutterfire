22a95085af63246fa0170acace101a126b5c7fba
Salakar <mike.diarmid@gmail.com>
2020-05-27 18:51:08 +0100

    refactor(ios): finalize iOS core rework

--- packages/firebase_core/firebase_core/darwin/Classes/FLTFirebasePlugin.m
+++ packages/firebase_core/firebase_core/darwin/Classes/FLTFirebasePlugin.m
@@ -4,6 +4,40 @@
 
 #import "FLTFirebasePlugin.h"
 
-// TODO(Salakar): Not currently used, will be used by future plugins.
+@interface FLTFirebaseMethodCallResult ()
+@property(readwrite, nonatomic) FLTFirebaseMethodCallErrorBlock error;
+@property(readwrite, nonatomic) FLTFirebaseMethodCallSuccessBlock success;
+@end
+@implementation FLTFirebaseMethodCallResult
+
++ (instancetype)createWithSuccess:(FLTFirebaseMethodCallSuccessBlock)successBlock andErrorBlock:(FLTFirebaseMethodCallErrorBlock)errorBlock {
+  FLTFirebaseMethodCallResult *methodCallResult = [[FLTFirebaseMethodCallResult alloc] init];
+  methodCallResult.error = errorBlock;
+  methodCallResult.success = successBlock;
+  return methodCallResult;
+}
+
+@end
+
 @implementation FLTFirebasePlugin
+- (FlutterError *)createFlutterErrorFromCode:(NSString *)code message:(NSString *)message optionalDetails:(NSDictionary *_Nullable)details andOptionalNSError:(NSError *_Nullable)error {
+  NSMutableDictionary *detailsDict = [NSMutableDictionary dictionaryWithDictionary:details ?: @{}];
+  if (error != nil) {
+    detailsDict[@"nativeErrorCode"] = [@(error.code) stringValue];
+    detailsDict[@"nativeErrorMessage"] = error.localizedDescription;
+  }
+  return [FlutterError errorWithCode:code message:message details:detailsDict];
+}
+
+- (NSString *)firebaseAppNameFromDartName:(NSString *)appName {
+  NSString *appNameIOS = appName;
+  if ([kFIRDefaultAppNameDart isEqualToString:appName]) {
+    appNameIOS = kFIRDefaultAppNameIOS;
+  }
+  return appNameIOS;
+}
+
+- (_Nullable FIRApp *)firebaseAppNamed:(NSString *)appName {
+  return [FIRApp appNamed:[self firebaseAppNameFromDartName:appName]];
+}
 @end
--- packages/firebase_core/firebase_core/ios/Classes/FLTFirebasePlugin.h
+++ packages/firebase_core/firebase_core/ios/Classes/FLTFirebasePlugin.h
@@ -6,6 +6,26 @@
 #import <Firebase/Firebase.h>
 #import <Flutter/Flutter.h>
 
+// Firebase default app name.
+NSString *const kFIRDefaultAppNameIOS = @"__FIRAPP_DEFAULT";
+NSString *const kFIRDefaultAppNameDart = @"[DEFAULT]";
+
+/**
+ * Block that is capable of sending a success response to a method call operation.
+ * Use this for returning success data to a Method call.
+ */
+typedef void (^FLTFirebaseMethodCallSuccessBlock)(id _Nullable result);
+
+/**
+ * Block that is capable of sending an error response to a method call operation.
+ * Use this for returning error information to a Method call.
+ */
+typedef void(^FLTFirebaseMethodCallErrorBlock)
+    (NSString *code, NSString *message, NSDictionary *_Nullable details, NSError _Nullable *error);
+
+/**
+ * A protocol that all FlutterFire plugins should implement.
+ */
 @protocol FLTFirebasePlugin<NSObject>
 /**
  * FlutterFire plugins implementing FLTFirebasePlugin must provide this method to provide it's
@@ -48,6 +68,58 @@
 - (NSString *)flutterChannelName;
 @end
 
-// TODO(Salakar): Not currently used, will be used by future plugins.
+/**
+ * An interface represent a returned result from a Flutter Method Call.
+ */
+@interface FLTFirebaseMethodCallResult : NSObject
++ (instancetype)createWithSuccess:(FLTFirebaseMethodCallSuccessBlock)successBlock andErrorBlock:(FLTFirebaseMethodCallErrorBlock)errorBlock;
+
+/**
+ * Submit a result indicating a successful method call.
+ *
+ * E.g.: `result.success(nil);`
+ */
+@property(readonly, nonatomic) FLTFirebaseMethodCallSuccessBlock success;
+
+/**
+ * Submit a result indicating a failed method call.
+ *
+ * E.g.: `result.error(@"code", @"message", nil);`
+ */
+@property(readonly, nonatomic) FLTFirebaseMethodCallErrorBlock error;
+
+@end
+
 @interface FLTFirebasePlugin : NSObject
+/**
+ * Creates a standardized instance of FlutterError using the values returned through FLTFirebaseMethodCallErrorBlock.
+ *
+ * @param code Error Code.
+ * @param message Error Message.
+ * @param details Optional dictionary of additional key/values to return to Dart.
+ * @param error Optional NSError that this error relates to.
+ *
+ * @return FlutterError
+ */
+- (FlutterError *)createFlutterErrorFromCode:(NSString *)code message:(NSString *)message optionalDetails:(NSDictionary *_Nullable)details andOptionalNSError:(NSError *_Nullable)error;
+
+/**
+ * Converts the '[DEFAULT]' app name used in dart and other SDKs to the '__FIRAPP_DEFAULT' iOS equivalent.
+ *
+ * If name is not '[DEFAULT]' then just returns the same name that was passed in.
+ *
+ * @param appName The name of the Firebase App.
+ *
+ * @return NSString
+ */
+- (NSString *)firebaseAppNameFromDartName:(NSString *)appName;
+
+/**
+ * Retrieves a FIRApp instance based on the app name provided from Dart code.
+ *
+ * @param appName The name of the Firebase App.
+ *
+ * @return FIRApp - returns nil if Firebase app does not exist.
+ */
+- (FIRApp _Nullable *)firebaseAppNamed:(NSString *)appName;
 @end

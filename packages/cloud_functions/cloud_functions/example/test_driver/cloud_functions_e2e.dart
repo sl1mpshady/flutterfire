4ff27659cb588a363ea0361c67385e574bfecadb
Salakar <mike.diarmid@gmail.com>
2020-06-01 11:23:36 +0100

    -

--- packages/cloud_functions/cloud_functions/example/test_driver/cloud_functions_e2e.dart
+++ packages/cloud_functions/cloud_functions/example/test_driver/cloud_functions_e2e.dart
@@ -1,3 +1,7 @@
+// Copyright 2020, the Chromium project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
 import 'package:e2e/e2e.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -6,8 +10,11 @@ import 'package:cloud_functions/cloud_functions.dart';
 void main() {
   E2EWidgetsFlutterBinding.ensureInitialized();
 
-  testWidgets('call', (WidgetTester tester) async {
+  setUpAll(() async {
     await FirebaseCore.instance.initializeApp();
+  });
+
+  testWidgets('call', (WidgetTester tester) async {
     final HttpsCallable callable =
         CloudFunctions.instance.getHttpsCallable(functionName: 'repeat');
     final HttpsCallableResult response = await callable.call(<String, dynamic>{

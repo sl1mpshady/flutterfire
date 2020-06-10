0a1d0b0191adece8a344a3c816b768a5debe3711
ehesp <elliot.hesp@gmail.com>
2020-06-10 15:36:28 +0100

    Update Firestore example

--- packages/cloud_firestore/cloud_firestore/example/lib/main.dart
+++ packages/cloud_firestore/cloud_firestore/example/lib/main.dart
@@ -2,158 +2,264 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  final FirebaseApp app = await FirebaseCore.instance.initializeApp(
-    name: 'test',
-    options: const FirebaseOptions(
-      appId: '1:79601577497:ios:5f2bcc6ba8cecddd',
-      messagingSenderId: '79601577497',
-      apiKey: 'AIzaSyArgmRGfB5kiQT6CunAOmKRVKEsxKmy6YI-G72PVU',
-      projectId: 'flutter-firestore',
-    ),
-  );
-  final Firestore firestore = Firestore(app: app);
-
-  runApp(MaterialApp(
-      title: 'Firestore Example', home: MyHomePage(firestore: firestore)));
+void main() {
+  runApp(App());
 }
 
-class MessageList extends StatelessWidget {
-  MessageList({this.firestore});
-
-  final Firestore firestore;
-
+class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<QuerySnapshot>(
-      stream: firestore
-          .collection("messages")
-          .orderBy("created_at", descending: true)
-          .snapshots(),
-      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-        if (!snapshot.hasData) return const Text('Loading...');
-        final int messageCount = snapshot.data.documents.length;
-        return ListView.builder(
-          itemCount: messageCount,
-          itemBuilder: (_, int index) {
-            final DocumentSnapshot document = snapshot.data.documents[index];
-            final dynamic message = document['message'];
-            return ListTile(
-              trailing: IconButton(
-                onPressed: () => document.reference.delete(),
-                icon: Icon(Icons.delete),
-              ),
-              title: Text(
-                message != null ? message.toString() : '<No message retrieved>',
-              ),
-              subtitle: Text('Message ${index + 1} of $messageCount'),
-            );
-          },
-        );
+    return FutureBuilder(
+      /// Initialize FlutterFire
+      future: FirebaseCore.instance.initializeApp(),
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return FirestoreExampleApp.error(snapshot.error.toString());
+        }
+        if (snapshot.connectionState == ConnectionState.done) {
+          return FirestoreExampleApp.ready(Firestore.instance);
+        }
+
+        return FirestoreExampleApp.loading();
       },
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  MyHomePage({this.firestore});
-
+class FirestoreExampleApp extends StatelessWidget {
+  final bool loading;
+  final String error;
   final Firestore firestore;
 
-  CollectionReference get messages => firestore.collection('messages');
+  FirestoreExampleApp._(
+      {this.loading = false, this.error = '', this.firestore});
 
-  Future<void> _addMessage() async {
-    await messages.add(<String, dynamic>{
-      'message': 'Hello world!',
-      'created_at': FieldValue.serverTimestamp(),
-    });
+  factory FirestoreExampleApp.loading() {
+    return FirestoreExampleApp._(loading: true);
   }
 
-  Future<void> _runTransaction() async {
-    firestore.runTransaction((Transaction transaction) async {
-      final allDocs = await firestore.collection("messages").get();
-      final toBeRetrieved =
-          allDocs.documents.sublist(allDocs.documents.length ~/ 2);
-      final toBeDeleted =
-          allDocs.documents.sublist(0, allDocs.documents.length ~/ 2);
-      await Future.forEach(toBeDeleted, (DocumentSnapshot snapshot) async {
-        await transaction.delete(snapshot.reference);
-      });
-
-      await Future.forEach(toBeRetrieved, (DocumentSnapshot snapshot) async {
-        await transaction.update(snapshot.reference, {
-          "message": "Updated from Transaction",
-          "created_at": FieldValue.serverTimestamp()
-        });
-      });
-    });
-
-    await Future.forEach(List.generate(2, (index) => index), (item) async {
-      await firestore.runTransaction((Transaction transaction) async {
-        await Future.forEach(List.generate(10, (index) => index), (item) async {
-          await transaction.set(firestore.collection("messages").document(), {
-            "message": "Created from Transaction $item",
-            "created_at": FieldValue.serverTimestamp()
-          });
-        });
-      });
-    });
+  factory FirestoreExampleApp.error(String error) {
+    return FirestoreExampleApp._(error: error);
   }
 
-  Future<void> _runBatchWrite() async {
-    final batchWrite = firestore.batch();
-    final querySnapshot = await firestore
-        .collection("messages")
-        .orderBy("created_at")
-        .limit(12)
-        .get();
-    querySnapshot.documents
-        .sublist(0, querySnapshot.documents.length - 3)
-        .forEach((DocumentSnapshot doc) {
-      batchWrite.updateData(doc.reference, {
-        "message": "Batched message",
-        "created_at": FieldValue.serverTimestamp()
-      });
-    });
-    batchWrite.setData(firestore.collection("messages").document(), {
-      "message": "Batched message created",
-      "created_at": FieldValue.serverTimestamp()
-    });
-    batchWrite.delete(
-        querySnapshot.documents[querySnapshot.documents.length - 2].reference);
-    batchWrite.delete(querySnapshot.documents.last.reference);
-    await batchWrite.commit();
+  factory FirestoreExampleApp.ready(Firestore firestore) {
+    return FirestoreExampleApp._(firestore: firestore);
+  }
+
+  MaterialApp withMaterialApp(Widget body) {
+    return MaterialApp(
+      title: 'Firestore Example App',
+      theme: ThemeData.dark(),
+      home: Scaffold(
+        body: body,
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
+    if (loading) {
+      return withMaterialApp(Center(child: CircularProgressIndicator()));
+    }
+
+    if (error.isNotEmpty) {
+      return withMaterialApp(Center(child: Text(error)));
+    }
+
+    return withMaterialApp(Center(child: FilmList(firestore)));
+  }
+}
+
+class FilmList extends StatelessWidget {
+  final Firestore firestore;
+
+  FilmList(this.firestore);
+
+  @override
+  Widget build(BuildContext context) {
+    Query query = firestore.collection('firestore-example-app');
+
+    /// Order by the year. Set [descending] to [false] to reverse the order
+    query = query.orderBy('year', descending: true);
+
+    /// Order by the score, and return only those which has one great than 90
+    // query = query.orderBy('score').where('score', isGreaterThan: 90);
+
+    /// Return the movies which have the following categories
+    // query = query.where('genre', arrayContainsAny: ['Fantasy', 'Sci-Fi']);
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Firestore Example'),
-        actions: <Widget>[
-          FlatButton(
-            onPressed: _runTransaction,
-            child: Text("Run Transaction"),
-          ),
-          FlatButton(
-            onPressed: _runBatchWrite,
-            child: Text("Batch Write"),
-          )
-        ],
-      ),
-      body: MessageList(firestore: firestore),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _addMessage,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+        appBar: AppBar(
+          title: Text('Firestore Example: Movies'),
+        ),
+        body: StreamBuilder<QuerySnapshot>(
+          stream: query.snapshots(),
+          builder: (context, stream) {
+            if (stream.connectionState == ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator());
+            }
+
+            if (stream.hasError) {
+              return Center(child: Text(stream.error.toString()));
+            }
+
+            QuerySnapshot querySnapshot = stream.data;
+
+            return ListView.builder(
+              itemCount: querySnapshot.size,
+              itemBuilder: (context, index) =>
+                  Movie(firestore, querySnapshot.documents[index]),
+            );
+          },
+        ));
+  }
+}
+
+class Movie extends StatelessWidget {
+  final Firestore firestore;
+  final DocumentSnapshot snapshot;
+
+  Movie(this.firestore, this.snapshot);
+
+  Map<String, dynamic> get movie {
+    return snapshot.data();
+  }
+
+  Widget get poster {
+    return Container(
+      width: 100,
+      child: Center(child: Image.network(movie['poster'])),
     );
+  }
+
+  Widget get details {
+    return Padding(
+        padding: EdgeInsets.only(left: 8, right: 8),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            title,
+            metadata,
+            genres,
+            Likes(
+              firestore: firestore,
+              reference: snapshot.reference,
+              currentLikes: movie['likes'],
+            )
+          ],
+        ));
+  }
+
+  Widget get title {
+    return Text("${movie['title']} (${movie['year']})",
+        style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold));
+  }
+
+  Widget get metadata {
+    return Padding(
+        padding: EdgeInsets.only(top: 8),
+        child: Row(children: [
+          Padding(
+              child: Text('Rated: ${movie['rated']}'),
+              padding: EdgeInsets.only(right: 8)),
+          Text('Runtime: ${movie['runtime']}'),
+        ]));
+  }
+
+  Widget get genres {
+    return Padding(
+        padding: EdgeInsets.only(top: 8),
+        child: Wrap(children: <Widget>[
+          for (String genre in movie['genre'])
+            Padding(
+              child: Chip(
+                  label: Text(genre, style: TextStyle(color: Colors.white)),
+                  backgroundColor: Colors.lightBlue),
+              padding: EdgeInsets.only(right: 2),
+            ),
+        ]));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: EdgeInsets.only(bottom: 4, top: 4),
+        child: Container(
+          child: Row(
+            children: [poster, Flexible(child: details)],
+          ),
+        ));
+  }
+}
+
+class Likes extends StatefulWidget {
+  final Firestore firestore;
+  final DocumentReference reference;
+  final num currentLikes;
+
+  Likes({Key key, this.firestore, this.reference, this.currentLikes})
+      : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return _Likes();
+  }
+}
+
+class _Likes extends State<Likes> {
+  num _likes;
+
+  _onLike(int current) async {
+    // Increment the "like" count straight away to show feedback to the user
+    setState(() {
+      _likes = current + 1;
+    });
+
+    try {
+      // Return and set the updated "likes" count from the transaction
+      int newLikes =
+          await widget.firestore.runTransaction<int>((transaction) async {
+        DocumentSnapshot txSnapshot = await transaction.get(widget.reference);
+
+        if (!txSnapshot.exists) {
+          throw Exception("Document does not exist!");
+        }
+
+        int updatedLikes = (txSnapshot.data()['likes'] ?? 0) + 1;
+        transaction.update(widget.reference, {'likes': updatedLikes});
+        return updatedLikes;
+      });
+
+      // Update the count once the transaction has completed
+      setState(() {
+        _likes = newLikes;
+      });
+    } catch (e) {
+      print("Failed to update likes for document! $e");
+
+      // If the transaction fails, revert back to the old count
+      setState(() {
+        _likes = current;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    int currentLikes = _likes ?? widget.currentLikes ?? 0;
+
+    return Row(children: [
+      IconButton(
+          icon: Icon(Icons.favorite),
+          iconSize: 20,
+          onPressed: () {
+            _onLike(currentLikes);
+          }),
+      Text("$currentLikes likes"),
+    ]);
   }
 }

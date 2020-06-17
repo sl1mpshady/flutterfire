8686aae6bb39c11e6b97c4386145b0d11477f237
Helena Ford <helenaellieford@gmail.com>
2020-06-17 16:39:44 +0100

    platform interface unit tests cleanup

--- packages/cloud_firestore/cloud_firestore_platform_interface/test/utils/test_firestore_message_codec.dart
+++ packages/cloud_firestore/cloud_firestore_platform_interface/test/utils/test_firestore_message_codec.dart
@@ -1,9 +1,9 @@
-// Copyright 2017, the Chromium project authors.  Please see the AUTHORS file
+// Copyright 2020, the Chromium project authors.  Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
-import 'package:cloud_firestore_platform_interface/cloud_firestore_platform_interface.dart';
 import 'package:flutter/foundation.dart';
 
+import 'package:cloud_firestore_platform_interface/cloud_firestore_platform_interface.dart';
 import 'package:cloud_firestore_platform_interface/src/method_channel/utils/firestore_message_codec.dart';
 
 /// This codec is able to decode FieldValues.

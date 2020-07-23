c56a9a53545676f4333c8df32591b9d7654c80ea
ehesp <elliot.hesp@gmail.com>
2020-07-23 11:21:16 +0100

    stub example app out

--- packages/firebase_auth/firebase_auth/example/lib/main.dart
+++ packages/firebase_auth/firebase_auth/example/lib/main.dart
@@ -4,9 +4,9 @@
 
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import './register_page.dart';
-import './signin_page.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
+// import './register_page.dart';
+// import './signin_page.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -19,63 +19,58 @@ class MyApp extends StatelessWidget {
   MyApp();
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Firebase Auth Demo',
-      home: MyHomePage(
-        title: 'Firebase Auth Demo',
-      ),
-    );
+    return MaterialApp(title: 'Firebase Auth Demo', home: Text("TODO"));
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({
-    Key key,
-    this.title,
-  }) : super(key: key);
+// class MyHomePage extends StatefulWidget {
+//   MyHomePage({
+//     Key key,
+//     this.title,
+//   }) : super(key: key);
 
-  final String title;
+//   final String title;
 
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
+//   @override
+//   _MyHomePageState createState() => _MyHomePageState();
+// }
 
-class _MyHomePageState extends State<MyHomePage> {
-  _MyHomePageState();
-  FirebaseAuth user;
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Container(
-            child: RaisedButton(
-              child: const Text('Test registration'),
-              onPressed: () => _pushPage(context, RegisterPage()),
-            ),
-            padding: const EdgeInsets.all(16),
-            alignment: Alignment.center,
-          ),
-          Container(
-            child: RaisedButton(
-              child: const Text('Test SignIn/SignOut'),
-              onPressed: () => _pushPage(context, SignInPage()),
-            ),
-            padding: const EdgeInsets.all(16),
-            alignment: Alignment.center,
-          ),
-        ],
-      ),
-    );
-  }
+// class _MyHomePageState extends State<MyHomePage> {
+//   _MyHomePageState();
+//   FirebaseAuth user;
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text(widget.title),
+//       ),
+//       body: Column(
+//         crossAxisAlignment: CrossAxisAlignment.start,
+//         children: <Widget>[
+//           Container(
+//             child: RaisedButton(
+//               child: const Text('Test registration'),
+//               onPressed: () => _pushPage(context, RegisterPage()),
+//             ),
+//             padding: const EdgeInsets.all(16),
+//             alignment: Alignment.center,
+//           ),
+//           Container(
+//             child: RaisedButton(
+//               child: const Text('Test SignIn/SignOut'),
+//               onPressed: () => _pushPage(context, SignInPage()),
+//             ),
+//             padding: const EdgeInsets.all(16),
+//             alignment: Alignment.center,
+//           ),
+//         ],
+//       ),
+//     );
+//   }
 
-  void _pushPage(BuildContext context, Widget page) {
-    Navigator.of(context).push(
-      MaterialPageRoute<void>(builder: (_) => page),
-    );
-  }
-}
+//   void _pushPage(BuildContext context, Widget page) {
+//     Navigator.of(context).push(
+//       MaterialPageRoute<void>(builder: (_) => page),
+//     );
+//   }
+// }

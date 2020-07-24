2e62181f9ab4a38fec01b6619134271d28f1adec
ehesp <elliot.hesp@gmail.com>
2020-07-24 08:28:31 +0100

    setup platform interface

---
+++ packages/firebase_crashlytics/firebase_crashlytics_platform_interface/lib/src/platform_interface/platform_interface_crashlytics.dart
@@ -0,0 +1,9 @@
+// Copyright 2020, the Chromium project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'package:plugin_platform_interface/plugin_platform_interface.dart';
+
+abstract class FirebaseCrashlyticsPlatform extends PlatformInterface {
+
+}
